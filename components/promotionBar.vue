1c4bd245f634b1255b0a8c5090dd6dae909bbdcd
Florian <25250981+BFlorian91@users.noreply.github.com>
2024-10-14 06:20:21 +0200

    feat ✨: add hover on the close promotion bar icon

--- components/promotionBar.vue
+++ components/promotionBar.vue
@@ -10,13 +10,13 @@ const handleShowPromotionBar = () => {
     <div
       class="bg-gradient-to-r from-[#4DC9E6] to-[#482c8f] text-gray-100 w-full sm:p-4 flex justify-between"
     >
-      <LucideConstruction class="w-6 h-6 text-gray-100 mr-2" />
+      <LucideConstruction tabindex="-1" class="w-6 h-6 text-gray-200 mr-2" />
       <div class="flex items-center gap-2">
         <p>Bienvenue ! Notre site évolue, mais vous pouvez déjà réserver vos formations !</p>
         <button
           @click="handleShowPromotionBar"
         >
-          <LucideX class="w-6 h-6 text-gray-100 cursor-pointer ml-2" />
+          <LucideX class="w-6 h-6 text-gray-100 cursor-pointer ml-2 hover:text-blue-400 transition-colors duration-300" />
         </button>
       </div>
     </div>
