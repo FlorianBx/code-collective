ff88b8bbae621589467def1edabdea96f8f4f791
Florian <25250981+BFlorian91@users.noreply.github.com>
2024-10-14 02:01:08 +0200

    refacto ♻️: harmonize color scheme for improved visual consistency

--- components/Feature.vue
+++ components/Feature.vue
@@ -31,13 +31,13 @@ const advantages: Advantage[] = [
     <div class="max-w-7xl mx-auto">
       <h2 class="text-3xl sm:text-4xl font-bold mb-8 sm:mb-16 text-center">
         <span class="bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-indigo-400">
-          WORKSHOPS INTENSIFS
+					Code Collective C'est quoi ?
         </span>
       </h2>
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8">
         <div v-for="(advantage, index) in advantages" :key="index" 
              class="relative bg-punk-surface rounded-lg shadow-md hover:shadow-xl transition-all duration-300 overflow-hidden group">
-          <div class="absolute top-0 left-0 w-full h-1 bg-gradient-to-r from-blue-500 to-indigo-500"></div>
+          <div class="absolute top-0 left-0 w-full h-1 bg-blue-400"></div>
           <div class="p-6 sm:p-8">
             <div class="w-12 h-12 sm:w-16 sm:h-16 mb-4 sm:mb-6 bg-gray-700 rounded-full flex items-center justify-center group-hover:bg-gray-600 transition-all duration-300">
               <component :is="advantage.icon" class="w-6 h-6 sm:w-8 sm:h-8 text-blue-400" />
@@ -45,15 +45,16 @@ const advantages: Advantage[] = [
             <h3 class="text-xl sm:text-2xl font-bold mb-3 sm:mb-4 text-gray-100">{{ advantage.title }}</h3>
             <p class="text-gray-300 group-hover:text-gray-100 transition-colors duration-300">{{ advantage.description }}</p>
           </div>
-          <div class="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-blue-500 to-indigo-500 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></div>
+          <div class="absolute bottom-0 left-0 w-full h-1 bg-gradient-to-r from-blue-400 to-indigo-500 transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300"></div>
         </div>
       </div>
-      <div class="mt-64 sm:mt-12 bg-punk-surface p-6 sm:p-8 rounded-lg shadow-md hover:shadow-xl transition-all duration-300 border-l-4 border-indigo-500">
+      <div class="relative group overflow-hidden mt-64 sm:mt-12 bg-punk-surface p-6 sm:p-8 rounded-lg shadow-md hover:shadow-xl transition-all duration-300 border-l-4 border-blue-400">
         <h3 class="text-xl sm:text-2xl font-bold mb-3 sm:mb-4 flex items-center text-gray-100">
-          <ShieldCheck class="w-6 h-6 sm:w-8 sm:h-8 mr-2 sm:mr-3 text-indigo-400" />
+          <ShieldCheck class="w-6 h-6 sm:w-8 sm:h-8 mr-2 sm:mr-3 text-blue-400" />
           Notre Engagement
         </h3>
         <p class="text-base sm:text-lg text-gray-300">Nous anticipons vos préoccupations et y répondons de manière proactive, garantissant ainsi une expérience d'apprentissage optimale. Notre approche pédagogique est conçue pour assurer votre réussite et votre satisfaction tout au long de votre parcours de formation.</p>
+      <div class="absolute bottom-0 right-0 w-1 h-full bg-gradient-to-b from-blue-400 to-indigo-500 transform scale-y-0 group-hover:scale-y-100 transition-transform duration-300"></div>
       </div>
     </div>
   </section>
