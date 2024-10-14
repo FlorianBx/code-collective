ce95509dac6beb6922901f46678344cafb5cf230
Florian <25250981+BFlorian91@users.noreply.github.com>
2024-10-14 03:43:03 +0200

    feat ✨: add grain effect to the background

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="relative bg-punk-global bg-[#111113] text-white min-h-screen">
+  <div class="relative bg-[#111113] text-white min-h-screen z-10">
+    <svg class="absolute -z-10 inset-0 w-full h-full">
+      <filter id="grainy">
+        <feTurbulence
+          type="fractalNoise"
+          baseFrequency="0.65"
+          stitchTiles="stitch"
+        />
+      </filter>
+      <rect width="100%" height="100%" filter="url(#grainy)" />
+    </svg>
     <header class="sticky top-0 backdrop-blur-sm bg-[#111113]/70 z-50">
       <PromotionBar />
       <NavBar />
@@ -9,3 +19,9 @@
     </main>
   </div>
 </template>
+
+<style scoped>
+svg {
+  opacity: 0.05;
+}
+</style>
